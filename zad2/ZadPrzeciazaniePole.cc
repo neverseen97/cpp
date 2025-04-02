94d04692bd90cb564abc642c4a383ea895a5c7e6
neverseen97 <neverseen1997@gmail.com>
2025-04-02 15:15:53 +0200

    zad 2

---
+++ zad2/ZadPrzeciazaniePole.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+double pole(double side)
+{
+    return side * side;
+}
+
+double pole(double a, double b)
+{
+    return a * b;
+}
+
+double pole(double r, bool isCircle)
+{
+    if (isCircle)
+    {
+        return M_PI * r * r;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    std::cout << "pole kwadratu o boku 4" << pole(4) << std::endl;
+    std::cout << "pole prostokata o bokach 4 i 5" << pole(4, 5) << std::endl;
+    std::cout << "pole kola o promieniu 4" << pole(4, true) << std::endl;
+    return 0;
+}
